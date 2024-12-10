4fdb61e639520c06a8f77988c081b973d1acbafd
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-12-10 17:05:03 +0300

    Сделал 1 задание из 7 лабораторной

--- lw7/Lw7/Lw7/Program.cs
+++ lw7/Lw7/Lw7/Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿using static System.Net.Mime.MediaTypeNames;
+
+string ReadFileNameFromConsole()
+{
+    Console.WriteLine( "Enter file name:" );
+
+    return Console.ReadLine()!;
+}
+
+List<char> ReadCharactersToDeleteFromConsole()
+{
+    Console.WriteLine( "Enter characters you want to delete, ... to end" );
+
+    string character = Console.ReadLine()!;
+    List<char> charactersToDelete = [];
+
+    while ( character != "..." )
+    {
+        charactersToDelete.Add( character[ 0 ] );
+
+        character = Console.ReadLine()!;
+    }
+
+    return charactersToDelete;
+}
+
+string ReadTextFromFile( string fileName )
+{
+    string text = "";
+
+    using ( StreamReader streamReader = new StreamReader( fileName ) )
+    {
+        text = streamReader.ReadToEnd();
+    }
+
+    return text;
+}
+
+string DeleteCharactersInText( string text, List<char> charactersToDelete )
+{
+    string newText = "";
+
+    for ( int i = 0; i < text.Length; i++ )
+    {
+        if ( !charactersToDelete.Contains( text[ i ] ) )
+        {
+            newText += text[ i ];
+        }
+    }
+
+    return newText;
+}
+
+async Task WriteTextToFile( string text, string fileName )
+{
+    using ( StreamWriter streamWriter = new StreamWriter( fileName ) )
+    {
+        streamWriter.Write( text );
+    }
+}
+
+string fileName = ReadFileNameFromConsole();
+List<char> charactersToDelete = ReadCharactersToDeleteFromConsole();
+string editedText = DeleteCharactersInText( ReadTextFromFile( fileName ), charactersToDelete );
+await WriteTextToFile( editedText, fileName );
